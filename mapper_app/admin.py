1c0b22166177e9672cc5f8ad7d5aec2f07496b81
birkin <birkin.diana@gmail.com>
2018-09-28 09:47:17 -0400

    implements admin.

--- mapper_app/admin.py
+++ mapper_app/admin.py
@@ -5,8 +5,15 @@ from django.contrib import admin
 
 
 class LocationMapperAdmin( admin.ModelAdmin ):
-    list_display = [ 'ils_code' ]
-    # ordering = [ '-count_date' ]
+    list_display = [ 'ils_code', 'las_code', 'notes', 'modify_date' ]
+    ordering = [ 'ils_code' ]
+    save_on_top = True
+
+class PickupAtMapperAdmin( admin.ModelAdmin ):
+    list_display = [ 'ils_code', 'las_code', 'notes', 'modify_date' ]
+    ordering = [ 'ils_code' ]
+    save_on_top = True
 
 
 admin.site.register( LocationMapper, LocationMapperAdmin )
+admin.site.register( PickupAtMapper, PickupAtMapperAdmin )
