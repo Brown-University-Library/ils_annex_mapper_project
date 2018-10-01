366107a70132f86feed1cfab4213b97bbff9e870
birkin <birkin.diana@gmail.com>
2018-10-01 11:43:46 -0400

    implements pickup-mapper response.

--- mapper_app/views.py
+++ mapper_app/views.py
@@ -2,12 +2,13 @@
 
 import datetime, json, logging, os, pprint
 from . import settings_app
-from mapper_app.lib import view_info_helper
+from .models import LocationMapper, PickupAtMapper
 from django.conf import settings as project_settings
 from django.contrib.auth import logout
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from mapper_app.lib import view_info_helper
 
 
 log = logging.getLogger(__name__)
@@ -28,8 +29,36 @@ def info( request ):
 
 
 def location_v2( request, ils_code ):
-    return HttpResponse( '`%s` perceived; location handler coming' % ils_code )
+    """ Returns json showing the annex-software `DELIVERY STOP` code for the given ILS `PICKUP AT` code. """
+    pickup_mapper_record = get_object_or_404(PickupAtMapper, ils_code=ils_code)
+    response_dct = {
+        'request': {
+            'requested_ils_code': ils_code
+        },
+        'result': {
+            'definition_ils_code': 'Sierra "PICKUP AT" code',
+            'definition_las_code': 'LAS "DELIVERY STOP" code',
+            'returned_las_code': pickup_mapper_record.las_code,
+            'service_documentation': settings_app.README_URL
+        }
+    }
+    output = json.dumps( response_dct, sort_keys=True, indent=2 )
+    return HttpResponse( output, content_type='application/json; charset=utf-8' )
 
 
 def pickup_v2( request, ils_code ):
-    return HttpResponse( '`%s` perceived; pickup handler coming' % ils_code )
+    """ Returns json showing the annex-software `DELIVERY STOP` code for the given ILS `PICKUP AT` code. """
+    pickup_mapper_record = get_object_or_404(PickupAtMapper, ils_code=ils_code)
+    response_dct = {
+        'request': {
+            'requested_ils_code': ils_code
+        },
+        'result': {
+            'definition_ils_code': 'Sierra "PICKUP AT" code',
+            'definition_las_code': 'LAS "DELIVERY STOP" code',
+            'returned_las_code': pickup_mapper_record.las_code,
+            'service_documentation': settings_app.README_URL
+        }
+    }
+    output = json.dumps( response_dct, sort_keys=True, indent=2 )
+    return HttpResponse( output, content_type='application/json; charset=utf-8' )
