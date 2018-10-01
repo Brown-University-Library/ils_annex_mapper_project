f5c4e929a2343069796b1709585f0c74d6bc8863
birkin <birkin.diana@gmail.com>
2018-10-01 13:40:43 -0400

    adds url for all locations.

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,8 @@ urlpatterns = [
 
     url( r'^info/$', views.info, name='info_url' ),
 
+    url( r'^location_api_v1/all/$',  views.location_all_v1, name='location_all_v1_url' ),
+
     url( r'^location_api_v2/ils_code_(?P<ils_code>.*)/$',  views.location_v2, name='location_v2_url' ),
 
     url( r'^pickup_api_v2/ils_code_(?P<ils_code>.*)/$',  views.pickup_v2, name='pickup_v2_url' ),
