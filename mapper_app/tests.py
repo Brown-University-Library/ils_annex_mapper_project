d614389689473b477d5534f34c82621d009706b6
birkin <birkin.diana@gmail.com>
2018-10-02 13:36:48 -0400

    adds test and fixture.

--- mapper_app/tests.py
+++ mapper_app/tests.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
-import logging
-from django.test import TestCase
+import json, logging
+from .models import LocationMapper
+from django.test import Client, TestCase
 # from django.test import SimpleTestCase as TestCase    ## TestCase requires db, so if you're not using a db, and want tests, try this
 
 
@@ -27,3 +28,34 @@ class RootUrlTest( TestCase ):
         self.assertEqual(  '/info/', redirect_url )
 
     # end class RootUrlTest()
+
+
+class LocationUrlTest( TestCase ):
+    """ Checks url hits. """
+    fixtures = [ 'location_dump.json' ]
+
+    def test_good_simple_location_hit(self):
+        """ Checks good non-space ils-code. """
+        response = self.client.get( '/location_api_v2/ils_code_ANNEX/' )  # project root part of url is assumed
+        # log.debug( 'response.content, ```%s```' % response.content )
+        self.assertEqual( 200, response.status_code )
+        las_code = json.loads( response.content )['result']['returned_las_code']
+        self.assertEqual( 'QS', las_code )
+
+    def test_not_found_location_hit(self):
+        """ Checks invalid ils-code. """
+        response = self.client.get( '/location_api_v2/ils_code_FOO/' )  # project root part of url is assumed
+        # log.debug( 'response.content, ```%s```' % response.content )
+        self.assertEqual( 404, response.status_code )
+
+    def test_good_location_hit_with_spaces(self):
+        """ Checks ils-code with spaces. """
+        response = self.client.get( '/location_api_v2/ils_code_ANNEX%20HAY/' )  # project root part of url is assumed
+        log.debug( 'response.content, ```%s```' % response.content )
+        self.assertEqual( 200, response.status_code )
+        submitted_code = json.loads( response.content )['request']['requested_ils_code']
+        self.assertEqual( 'ANNEX HAY', submitted_code )
+        las_code = json.loads( response.content )['result']['returned_las_code']
+        self.assertEqual( 'QH', las_code )
+
+    # end class LocationUrlTest()
