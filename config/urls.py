f87e741350d57a0fedc420a51a2722d8c518af77
birkin <birkin.diana@gmail.com>
2018-11-19 15:33:50 -0500

    adds auth urls.

--- config/urls.py
+++ config/urls.py
@@ -11,8 +11,9 @@ admin.autodiscover()
 
 urlpatterns = [
 
-    url( r'^admin/', admin.site.urls ),  # eg host/project_x/admin/
-    # url( r'^admin/', include(admin.site.urls) ),
+    url( r'^admin/login/', RedirectView.as_view(pattern_name='login_url') ),
+    url( r'^admin/', admin.site.urls ),
+    url( r'^login/$', views.login, name='login_url' ),
 
     url( r'^info/$', views.info, name='info_url' ),
 
