9a3aff688c26663a8f7ad0a698bd69285530df8e
birkin <birkin.diana@gmail.com>
2018-11-19 15:34:06 -0500

    adds auth-settings.

--- mapper_app/settings_app.py
+++ mapper_app/settings_app.py
@@ -4,3 +4,10 @@ import json, os
 
 
 README_URL = os.environ['ILS_ANX_MPR__README_URL']
+
+## auth
+SUPER_USERS = json.loads( os.environ['ILS_ANX_MPR__SUPER_USERS_JSON'] )
+STAFF_USERS = json.loads( os.environ['ILS_ANX_MPR__STAFF_USERS_JSON'] )  # users permitted access to admin
+STAFF_GROUP = os.environ['ILS_ANX_MPR__STAFF_GROUP']  # not grouper-group; rather, name of django-admin group for permissions
+TEST_META_DCT = json.loads( os.environ['ILS_ANX_MPR__TEST_META_DCT_JSON'] )
+POST_LOGIN_ADMIN_REVERSE_URL = os.environ['ILS_ANX_MPR__POST_LOGIN_ADMIN_REVERSE_URL']
